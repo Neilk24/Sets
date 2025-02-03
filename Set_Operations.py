243cbaa2da833509fe9db228f80a0f81729fcb97
Neilk24 <neilk2050@gmail.com>
2025-02-03 18:30:51 -0500

    Add files via upload

---
+++ Set_Operations.py
@@ -0,0 +1,16 @@
+my_set = {1,2,3,4,5,6,7,8,9,0}
+print(my_set)
+
+my_set = (2.3333435, "Hello, sir", (1,2,3,4,5,6,7,8,9,0))
+print(my_set)
+
+my_set = {1,2,3,4,5,6,7,8,9,0,9,8,7,6,5,4,3,2,1}
+print(my_set)
+
+my_set = set([1,2,3,4,5,5,4,3,2,1,6,7,8,9,0])
+print(my_set)
+
+my_set = {1,6,7,8,9}
+print('Original set is: ', my_set, '\n')
+my_set.pop()
+print('New set is: ', my_set, '\n')
