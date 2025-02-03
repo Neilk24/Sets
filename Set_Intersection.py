1dd301fb54bafd1c3b2476833568c2a1bf499f53
Neilk24 <neilk2050@gmail.com>
2025-02-03 18:37:26 -0500

    Add files via upload

---
+++ Set_Intersection.py
@@ -0,0 +1,10 @@
+set_x = {1,2,3}
+set_y = {-1, 0, 1}
+
+print('Original sets:\n ')
+print(set_x, '\n')
+print(set_y, '\n')
+
+set_z = set_x.intersection(set_y)
+
+print('Intersection of the two sets:', set_z, '\n')
